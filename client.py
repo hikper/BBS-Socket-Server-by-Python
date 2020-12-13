9a93f11f92f48b25cc630e054f081cf821955492
hikper <hikper@gmail.com>
2020-12-13 20:58:22 +0800

    fix bug

--- client.py
+++ client.py
@@ -15,29 +15,26 @@ argp.add_argument("port")
 args = argp.parse_args()
 host = str(args.host)
 port = int(args.port)
-print(f"Server host: {host}, port: {port}")
+
 
 
 udpserver = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 udpserver.connect((host,port))
-print("UDP Server connect success")
-
-
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print("Start to connect")
 server.connect((host,port))
-print("Connected Success")
 print(server.recv(1024).decode())
 
 rannum = ""
-
 chatter = list()
 history = list()
-
 attach = False
 server_running = False
+server_running_joinother = False
 chatroom = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+chatroom_addr = str()
+chatroom_port = int()
+myname = str()
 
 def get_time():
     today = datetime.now()
@@ -50,32 +47,42 @@ class chatroom_server(threading.Thread):
         self.client = client
         self.address = address
     def run(self):
-        welcome_message = "***************************\n**Welcome to the chatroom**\n***************************"
-        for it in history:
-            welcome_message += "\n"+it
-        self.client.send(welcome_message.encode())
-        # print(f"server is runnung here!")
-        while server_running == True:
-            message = self.client.recv(1024).decode()
-            # print(f"receive mes! {message}")
-            if "leave-chatroom" in message:
-                break
-            if "sys" not in message:
-                history.append(message)
-            while len(history) > 3:
-                history.pop(0)
-            for client in chatter:
-                if client == self.client:
-                    # print(f"myself")
-                    continue
-                else:
-                    # print(f"not myself {message}")
-                    client.send(message.encode())
-            if "leave us." in message:
-                chatter.remove(self.client)
-                self.client.close()
-                break
-
+        global attach,server_running
+        try:
+            welcome_message = "***************************\n**Welcome to the chatroom**\n***************************"
+            for it in history:
+                welcome_message += "\n"+it
+            self.client.send(welcome_message.encode())
+            while server_running == True:
+                message = self.client.recv(1024).decode()
+                # print(f"receive: {message}")
+                if "leave-chatroom" in message:
+                    break
+                if "sys" not in message and message != "":
+                    history.append(message)
+                while len(history) > 3:
+                    history.pop(0)
+                if myname in message and "leave us" in message:
+                    self.client.close()
+                    break
+                for client in chatter:
+                    if client == self.client:
+                        continue
+                    else:
+                        client.send(message.encode())
+                if "leave us." in message:
+                    self.client.close()
+                    break
+                if "the chatroom is close" in message:
+                    attach = False
+                    server_running = False
+                    break
+        except BrokenPipeError:
+            pass
+        try:
+            chatter.remove(self.client)
+        except:
+            pass
 class chatroom_server_set_up(threading.Thread):
     def __init__(self,address,port):
             threading.Thread.__init__(self)
@@ -87,14 +94,13 @@ class chatroom_server_set_up(threading.Thread):
             try:
                 chatroom.bind((self.host, self.port))
             except OSError:
-                asdasdasd = int()
+                pass
             chatroom.listen(20)
-            # print("chatroom_server_ready")
             while server_running == True:
                 client, addr = chatroom.accept()
                 chatroom_server(client,addr).start()
-                # client.send("wewewewe".encode())
                 chatter.append(client)
+            chatroom.close()
 
 class chatroom_listen(threading.Thread):
     def __init__(self,chatroom_server):
@@ -104,18 +110,21 @@ class chatroom_listen(threading.Thread):
         global attach
         global chatroom
         while attach == True:
-            response = self.chatroom_server.recv(1024).decode()
-            if response != "":
-                print(response)
-            if "the chatroom is close" in response:
-                attach = False
-                chatroom.close()
-                chatroom = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            try:
+                response = self.chatroom_server.recv(1024).decode()
+                if attach == False:
+                    break
+                if response != "":
+                    print(response)
+                if "the chatroom is close" in response:
+                    print("Welcome back to BBS.")
+                    attach = False
+                    chatroom.close()
+                    chatroom = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                    break
+            except:
                 break
-        # print(f"lister is end!!!")
 
-
-myname = str()
 while True :
     if attach == False:
         print("% ",end="")
@@ -126,10 +135,10 @@ while True :
                 print("Please login first.")
             if server_running == False:
                 print("Please create-chatroom first.")
-            print("***************************\n**Welcome to the chatroom**\n***************************")
-            for item in history:
-                print(item)
             attach = True
+            chatroom = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            chatroom.connect((chatroom_addr,chatroom_port))
+            chatroom_listen(chatroom).start()
         elif "register" in cmd or "whoami" in cmd:
             if "whoami" in cmd:
                 cmd = cmd + f" {rannum}"
@@ -146,54 +155,61 @@ while True :
                 if len(response) > 1:
                     rannum = response[1]
                 response = response[0]
-            if response == "exit":
+            elif response == "exit":
                 server.close()
                 break
-            if "start to create chatroom" in response:
+            elif "start to create chatroom" in response:
                 attach = True
                 chatroom_addr = response.split(" ")[4]
                 chatroom_port = int(response.split(" ")[5])
-                # print("start to create chatroom…")
                 chatroom_server_set_up(chatroom_addr,chatroom_port).start()
                 server_running = True
                 time.sleep(0.1)
                 chatroom.connect((chatroom_addr,chatroom_port))
                 chatroom_listen(chatroom).start()
                 continue
-            if "Action: connection to chatroom server." in response:
-                print("Action: connection to chatroom server")
+            elif "Action: connection to chatroom server." in response:
+                if server_running == True:
+                    server_running_joinother = True
                 attach = True
                 chatroom.connect((response.split()[5],int(response.split()[6])))
                 chatroom_listen(chatroom).start()
                 chatroom.send(f"sys [{get_time()}] : {myname} join us.".encode())
-                
                 continue
-
             print(response)
     else:
         try:
             if "detach" == cmd and server_running == True:
                 print("Welcome back to BBS.")
+                chatroom.send(f"sys [{get_time()}] : {myname} leave us.".encode())
+                chatroom.close()
+                chatroom = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
                 attach = False
             elif "leave-chatroom" == cmd:
-                if server_running == True:
+                # print(f"{server_running} , {server_running_joinother}")
+                if server_running == True and server_running_joinother == False:
                     server.send(cmd.encode())
                     response = server.recv(1024).decode()
+                    # print(f"doing shutdown server chatroom process")
                     chatroom.send(f"sys [{get_time()}] : the chatroom is close".encode())
                     chatroom.close()
                     chatroom = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
                     chatter = list()
+                    server_running = False
                 else:
-                    chatroom.send(f"sys [{get_time()}] : {myname} leave us.".encode())
+                    if server_running == True and server_running_joinother == False:
+                        pass
+                    else:
+                        chatroom.send(f"sys [{get_time()}] : {myname} leave us.".encode())
                     chatroom.close()
                     chatroom = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-                server_running = False
+                server_running_joinother = False
                 attach = False
+                print("Welcome back to BBS.")
             else:
                 cmd = f"{myname}[{get_time()}]: {cmd}"
                 # print(f"check send mes {cmd}")
                 chatroom.send(cmd.encode())
         except BrokenPipeError:
             attach = False
-            print("Chatroom connection has been lost")
         
